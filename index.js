6d762d835df259a76094130bb680078d0d43545f
Maryannewambui <trimbleleewambui@gmail.com>
2023-09-19 00:00:41 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+//multiply
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+console.log(multiply)
+
+//random
+const random = parseInt(Math.random(3, 1000)*100)
+console.log(random)
+
+//mod
+const num3 = 15;
+const num4 = 11;
+
+const mod = num3 % num4;
+console.log(mod)
+
+//max
+const set = [15, 19, 20];
+const max = Math.max(...set);
+
+console.log(max);
